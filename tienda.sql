a8f068aa237d2cddc55db62e1a63a6f76379172d
antoniolc11 <antoniolc11@hotmail.com>
2022-11-07 19:25:00 +0100

    crea tienda.sql

--- tienda.sql
+++ tienda.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS articulos CASCADE;
 CREATE TABLE articulos (
     id bigserial    PRIMARY KEY,
     codigo          VARCHAR(13) NOT NULL UNIQUE,
-    descripcion     VARCHAR(255) NOT NULL,BIGINT
-    precio          numeric(7.2) NOT NULL
+    descripcion     VARCHAR(255) NOT NULL,
+    precio          numeric(7,2) NOT NULL
 );
 
 --Carga inicial datos de prueba
 
 INSERT INTO articulos (codigo, descripcion, precio)
     VALUES ('3216546546541', 'Yogur piña', 200.50),
-           ('1213546546544', 'Tigreton', 50.10),
+           ('1213546546544', 'Tigreton',   50.10),
            ('1234567897898', 'Disco duro', 150.30);
